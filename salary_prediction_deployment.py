dce817db37e36bb92a7b7025cc4c79d171180c19
vishwakaramamanish897-alt <vishwakaramamanish897@gmail.com>
2026-03-10 15:11:02 +0530

    Fix variable naming for job title and experience

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -20,8 +20,8 @@ st.title("salary prediction model")
 Age = st.number_input("Enter your age",18,65)
 Gender = st.selectbox("select your gender",encoder["Gender"].classes_)
 Education = st.selectbox("select your enduction",encoder["Education Level"].classes_)
-job_title = st.selectbox("select your job title",encoder["Job Title"].classes_)
-experience = st.number_input("enter your experience(in year)",0,50)
+Job Title = st.selectbox("select your job title",encoder["Job Title"].classes_)
+Experience = st.number_input("enter your experience(in year)",0,50)
 
 
 df = pd.DataFrame({
